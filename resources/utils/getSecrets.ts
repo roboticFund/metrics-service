59351d745c106456ddbc4d232ec2eebcb85433f8
Robert Cicero <robert.cicero@hotmail.com>
2023-10-07 20:23:00 +0100

    Trying to pass secrets effeciently

--- resources/utils/getSecrets.ts
+++ resources/utils/getSecrets.ts
@@ -3,17 +3,18 @@ import { SecretsManagerClient, GetSecretValueCommand } from "@aws-sdk/client-sec
 export default async function getCustomerCredentials(): Promise<any> {
   const secret_name = "customer-broker-credentials";
   const client = new SecretsManagerClient();
-
+  let secretString: string;
   let response;
+  let secretAsObject: any;
 
   try {
     response = await client.send(new GetSecretValueCommand({ SecretId: secret_name }));
+    secretString = response.SecretString as string;
+    secretAsObject = JSON.parse(secretString);
+    console.log(`Secret is ${secretAsObject}`);
   } catch (error) {
-    // For a list of exceptions thrown, see
-    // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
     throw error;
   }
-  const secret = response.SecretString;
-  console.log(`Secret is ${secret}`);
-  return secret;
+
+  return secretAsObject;
 }
