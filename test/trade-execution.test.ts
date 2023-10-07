336e0e90791834b9f092de43478ec66ce6e1165d
Robert Cicero <robert.cicero@hotmail.com>
2023-10-07 11:33:02 +0100

    Initial commit

---
+++ test/trade-execution.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TradeExecution from '../lib/trade-execution-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/trade-execution-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TradeExecution.TradeExecutionStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
