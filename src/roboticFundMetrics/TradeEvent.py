eded1bfae4c25c893bf6c29432b2e9f357d18fce
Robert Cicero <robert.cicero@hotmail.com>
2023-11-26 12:47:36 +0000

    Added stop and limit to new trade event

--- src/roboticFundMetrics/TradeEvent.py
+++ src/roboticFundMetrics/TradeEvent.py
@@ -1,16 +1,19 @@
 # Class must match Schema definition in ../lib/trade-decision-engine.ts
 import json
 
+
 class TradeEvent:
 
-    def __init__(self, datetime, inputEvent, accountName, instrument, direction, action):
+    def __init__(self, datetime, inputEvent, accountName, instrument, direction, action, stop, limit):
         self.datetime = datetime
         self.inputEvent = inputEvent
         self.accountName = accountName
         self.instrument = instrument
         self.direction = direction
         self.action = action
-    
+        self.stop = stop
+        self.limit = limit
+
     def returnSnsFormat(self):
         return json.dumps({
             "datetime": self.datetime,
@@ -18,5 +21,7 @@ class TradeEvent:
             "accountName": self.accountName,
             "instrument": self.instrument,
             "direction": self.direction,
-            "action": self.action
+            "action": self.action,
+            "stop": self.stop,
+            "limit": self.limit
         })
--- src/roboticFundMetrics/emitNewTradeEvent.py
+++ src/roboticFundMetrics/emitNewTradeEvent.py
@@ -3,9 +3,9 @@ from .TradeEvent import TradeEvent
 import boto3
 
 
-def emitNewTradeEvent(datetime, inputEvent, accountName, instrument, direction, action):
+def emitNewTradeEvent(datetime, inputEvent, accountName, instrument, direction, action, stop, limit):
     client = boto3.client('sns')
     tradeEvent = TradeEvent(datetime, inputEvent,
-                            accountName, instrument, direction, action)
+                            accountName, instrument, direction, action, stop, limit)
 
     return client.publish(TargetArn="arn:aws:sns:ap-southeast-2:302826945104:TradeDecisionEngine-newTradeEventA11BB639-ZEoklthJHhYP", Message=tradeEvent.returnSnsFormat(), MessageStructure='json')
