e66d86bc5bdb9d5d5f3bea9339e4fc5f1f088d44
Robert Cicero <robert.cicero@hotmail.com>
2024-01-31 16:49:41 +1100

    Added CI backtest capability

--- src/roboticFundMetrics/CI.py
+++ src/roboticFundMetrics/CI.py
@@ -1,6 +1,9 @@
 import requests
 import boto3
 from .utils.market_id_resolver import Broker_Id_Resolver
+from .dbConnect import dbConnect
+import datetime
+import pandas
 
 
 class CI:
@@ -57,3 +60,45 @@ class CI:
             f"{self.ci_url}/market/search?SearchByMarketName=TRUE&Query={market_name}&MaxResults=10", headers=self.headers)
         response_json = r.json()
         print(response_json)
+
+    def get_market_data_history(self, market_name, year=2023, month=1):
+        print(
+            f"Getting market data history from CI for {market_name} for year {year} and month {month}")
+        epic = Broker_Id_Resolver(market_name).return_ci_epic()
+        print(f"CI market id for {market_name} is {epic}")
+        from_date = datetime.date(year, month, 1).strftime("%s")
+        to_date = datetime.date(year, month+1, 1).strftime("%s")
+        r = requests.get(
+            f"{self.ci_url}/market/{epic}/barhistorybetween?interval=MINUTE&span=15&fromTimestampUTC={from_date}&toTimestampUTC={to_date}", headers=self.headers)
+        response_json = r.json()
+        market_data = pandas.DataFrame.from_dict(response_json['PriceBars'])
+        market_data['BarDateUTCNumber'] = market_data['BarDate'].str.extract(
+            '(\d+)')
+        market_data['snapshotTimeUTC'] = pandas.to_datetime(
+            market_data['BarDateUTCNumber'], utc=True, unit='ms').dt.round('5min')
+        market_data = market_data.rename(columns={
+                                         'Open': 'openPrice', 'High': 'highPrice', 'Low': 'lowPrice', 'Close': 'closePrice', 'volume': 'volume'})
+        market_data['instrument'] = market_name
+        market_data['datetime'] = market_data['snapshotTimeUTC']
+        market_data['insertStamp'] = datetime.datetime.now()
+        market_data['resolution'] = "15M"
+        market_data = market_data[market_data['openPrice'].notna()]
+        market_data = market_data.set_index('snapshotTimeUTC')
+        print(market_data.head())
+        # Drop unwanted columns
+        market_data = market_data.drop(
+            ['BarDateUTCNumber', 'BarDate'], axis=1)
+
+        return market_data
+
+    def store_market_data_history_in_db(instrument, market_name, year=2023):
+        con = dbConnect()
+        df = pandas.DataFrame()
+        for i in range(1, 12):
+            market_data = ci.get_market_data_history(
+                market_name, year=year, month=i)
+            df = pandas.concat([df, market_data], axis=0)
+
+        df = df.drop_duplicates(
+            subset=['instrument', 'resolution', 'datetime'])
+        df.to_sql('market_data', con, if_exists='append')
