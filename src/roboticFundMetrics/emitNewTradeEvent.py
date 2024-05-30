70804f169933eed9d8ec881844f4d83ded26bf27
Robert Cicero <robert.cicero@hotmail.com>
2024-05-30 14:14:58 +0100

    Improvement for local dev

--- src/roboticFundMetrics/emitNewTradeEvent.py
+++ src/roboticFundMetrics/emitNewTradeEvent.py
@@ -5,8 +5,12 @@ import os
 
 
 def emitNewTradeEvent(datetime, inputEvent, accountName, algoName, secretName, instrument, direction, action, stop, limit, current_price, size):
-    client = boto3.client('sns')
     tradeEvent = TradeEvent(datetime, inputEvent,
                             accountName, algoName, secretName, instrument, direction, action, stop, limit, current_price, size)
     print(f"Trade event is {tradeEvent.returnSnsFormat()}")
-    return client.publish(TargetArn=os.getenv("TRADE_EVENT_TOPIC_ARN"), Message=tradeEvent.returnSnsFormat())
+    if (os.environ.get("ENV").lower() == "dev"):
+        print("Running in dev, not sending to AWS...")
+        return
+    else:
+        client = boto3.client('sns')
+        return client.publish(TargetArn=os.getenv("TRADE_EVENT_TOPIC_ARN"), Message=tradeEvent.returnSnsFormat())
